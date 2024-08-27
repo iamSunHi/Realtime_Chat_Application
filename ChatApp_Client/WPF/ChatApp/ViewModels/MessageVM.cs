f3619414a9fc8490f15c9d641f15a13876772757
NhatHuy <doan.nhathuy.03@gmail.com>
2024-08-27 14:59:02 +0700

    Add Message component

---
+++ ChatApp_Client/WPF/ChatApp/ViewModels/MessageVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.ViewModels
+{
+	public class MessageVM
+	{
+		public string Time { get; set; } = null!;
+		public string SenderName { get; set; } = null!;
+		public string MessageText { get; set; } = null!;
+		public bool IsOwner { get; set; } = false;
+	}
+}
